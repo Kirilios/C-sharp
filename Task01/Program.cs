b16d1c33ea70bb6fab254a83f42510deb52cfe83
Kirilios <kirilios17@mail.ru>
2023-08-01 12:49:22 +0300

    update

---
+++ Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+
+Console.WriteLine("Enter the number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 == number2*number2)
+{
+    Console.WriteLine("True");
+}
+else
+{
+    Console.WriteLine("False");
+}
---
+++ Task07/Program.cs
@@ -0,0 +1,17 @@
+﻿/*07. # Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+Console.WriteLine("Enter your number ");
+int n =Convert.ToInt32(Console.ReadLine());
+if (n  > 99 && n <1000)
+{
+    Console.WriteLine(n%10);
+}
+else
+{
+    Console.WriteLine("There aren't 3 numbers in the number");
+}
