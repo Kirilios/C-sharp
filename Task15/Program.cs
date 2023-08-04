845f4e033fd91c6f6b086214b95c58eec6bbd72c
Kirilios <kirilios17@mail.ru>
2023-08-04 23:40:55 +0300

    changes

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Напишите программу, которая принимает на вход цифру, 
+обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("Enter your number ");
+int number =Convert.ToInt32(Console.ReadLine());
+if (number > 7)
+{
+    Console.WriteLine("invalid number");
+}
+if (number <= 5 && number >= 1 )
+{
+    Console.WriteLine("Weekday");
+}
+if (number > 5)
+{
+    Console.WriteLine("it's a weekend");
+}
