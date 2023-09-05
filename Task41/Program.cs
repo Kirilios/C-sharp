67736095ea229e72b16e127d46178bbfa066829b
Kirilios <kirilios17@mail.ru>
2023-09-05 11:27:40 +0300

    new tasks2

---
+++ Task41/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Пользователь вводит с клавиатуры M чисел.
+ Посчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+ Console.Clear();
+
+int[] array = MyArray();
+PrintArray(array);
+Console.Write(" -> " + PositiveCount(array));
+
+int[] MyArray()
+{
+    Console.Write("Укажите длинну массива: ");
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Элемент {i} из {length - 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+int PositiveCount(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) result++;
+    }
+    return result;
+}
--- experimental/Program.cs
+++ experimental/Program.cs
@@ -126,7 +126,7 @@ for (int i = 0; i < 40; i++)
 // f(2) = 1
 // f(n) = f(n-1) + f(n-2)
 
-int Fibonacci(int n)
+/*int Fibonacci(int n)
 {
     if (n == 1 || n == 2) return 1;
     else return Fibonacci(n - 1) + Fibonacci(n - 2);
@@ -134,4 +134,83 @@ int Fibonacci(int n)
 for (int i = 0; i < 10; i++)
 {
     Console.WriteLine(Fibonacci(i));
+}*/
+
+/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+//дз
+Console.Clear();
+
+int[] array = MyArray();
+PrintArray(array);
+Console.Write(" -> " + PositiveCount(array));
+
+int[] MyArray()
+{
+    Console.Write("Укажите длинну массива: ");
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Элемент {i} из {length - 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
 }
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+int PositiveCount(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) result++;
+    }
+    return result;
+}
+/*
+* Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+значения b1, k1, b2 и k2 задаются пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+*/
+
+Console.Clear();
+
+
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+/*
+Упростим (решим) уравнение, для получения координат:
+y = k1 * x + b1, y = k2 * x + b2
+k1 * x + b1 = k2 * x + b2 (переносим влево значения с Х и выносим Х за скобки)
+(k1 - k2) * x = b2 - b1 (делим обе части на (k1 - k2))
+x = (b2 - b1) / (k1 - k2)
+y = k1 * x + b1
+*/
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.WriteLine($"Точка пересечения прямых находится в координатах: x={x}; y={y}]");
